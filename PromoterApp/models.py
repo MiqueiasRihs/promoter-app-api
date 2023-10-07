513b5254dfab9e4ebb1678a87f020e776224fc91
Miqueias <miqueias.rihs@gmail.com>
2023-10-06 21:28:55 -0300

    show product image

--- PromoterApp/models.py
+++ PromoterApp/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     def image_tag(self):
         return mark_safe('<img src="/media/%s" height="150" />' % (self.image))
 
-    image_tag.short_description = 'Image'
+    image_tag.short_description = 'Imagem'
 
     class Meta:
         verbose_name = u"Produto"

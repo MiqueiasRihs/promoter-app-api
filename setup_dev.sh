e6f7c9bb8fa93463a24b2ed7cdceae52bfab357f
Miqueias <miqueias.rihs@gmail.com>
2023-10-07 16:30:26 -0300

    database setup

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,32 +1,26 @@
 #!/bin/sh
 
-
 attempts=1
 max_attempts=10
 
 while [ $attempts -lt $max_attempts ]; do
     if pg_isready -h tradex_postgres -p 5432 -U postgres -d tradex_promoter; then
-        echo -e "\n================ Banco de dados PostgreSQL está pronto! ================\n"
-        
-        echo ">>> Executando migrações..."
+        echo "\n================ Banco de dados está pronto, executando migrações... ================\n"
         python3 manage.py makemigrations
         python3 manage.py migrate
         python3 manage.py createcachetable
 
-        echo ">>> Criando superusuário..."
+        echo "\n================ Criando superusuário... ================\n"
         python3 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
-        # echo ">>> Populando a tabela SetUserFormConfiguration..."
-        # python3 manage.py ONEOFF_set_user_form_configuration
+        echo "\n================ Iniciando os testes unitários... ================\n"
+        python3 manage.py test
 
-        # echo ">>> Iniciando os testes unitários..."
-        # python -m pytest
-
-        echo ">>> Iniciando o servidor Django..."
+        echo "\n================ Iniciando o servidor Django... ================\n"
         python3 manage.py runserver 0.0.0.0:8000
         break
     else
-        echo -e "\nTentativa $attempts: Banco de dados não está pronto. Aguarde um momento, reconectando...\n"
+        echo "\nTentativa $attempts: Banco de dados não está pronto. Aguarde um momento, reconectando...\n"
         attempts=$((attempts+1))
         sleep 5
     fi
